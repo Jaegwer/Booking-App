ac8ac80fb64e0474f9cc7b882c00a39812c99c9b
Jaegwer <ucet2696@gmail.com>
2024-02-06 02:03:22 +0300

    calendar updates

--- components/PlayCalender.vue
+++ components/PlayCalender.vue
@@ -1,28 +1,53 @@
-<script>
-import FullCalendar from '@fullcalendar/vue3'
-import dayGridPlugin from '@fullcalendar/daygrid'
+<script setup>
+
+import { ref, onMounted } from "vue";
+import { getFirestore, collection, getDocs } from "firebase/firestore";
+import FullCalendar from "@fullcalendar/vue3";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import trLocale from "@fullcalendar/core/locales/tr";
 
 
-export default {
-  components: {
-    FullCalendar
-  },
-  data: function() {
-    return {
-      calendarOptions: {
-        plugins: [dayGridPlugin],
-        initialView: 'dayGridMonth',
-        weekends: true,
-        events: [
-          { title: 'Meeting', start: new Date() }
-        ],
-		locale: 'tr'
-      }
-    }
+
+
+
+const formatTimestamp = (timestamp) => {
+  const milliseconds = timestamp.seconds * 1000 + timestamp.nanoseconds / 1e6;
+  const dateObject = new Date(milliseconds);
+  const year = dateObject.getFullYear();
+  const month = dateObject.getMonth() + 1;
+  const day = dateObject.getDate();
+  const hours = dateObject.getHours();
+const minutes = dateObject.getMinutes();
+  return `${year}-${month < 10 ? "0" + month : month}-${
+    day < 10 ? "0" + day : day
+  } ${hours}:${minutes}`;
+};
+const calendarOptions = {
+  plugins: [dayGridPlugin],
+  initialView: "dayGridMonth",
+  locale: trLocale,
+  events: async function (info, successCallback, failureCallback) {
+    const db = getFirestore();
+    const q = collection(db, "Calendar");
+     querySnapshot = await getDocs(q);
+    let events = [];
+
+    querySnapshot.docs.forEach((doc) => {
+      events.push({ title: doc.title,
+			start: formatTimestamp(doc.date),
+		});
+    });
+
+    successCallback(events);
   }
-}
-</script>
+};
 
+
+onMounted(async() => {
+
+  console.log(calendarOptions.events);
+});
+</script>
 <template>
 	<div class="bg-black">
 		<FullCalendar :options='calendarOptions' />
